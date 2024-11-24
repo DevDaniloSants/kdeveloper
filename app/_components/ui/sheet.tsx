3c3d22f64b42eac0894a4de5169c5f24ce5e8071
Danilo Santos <dev.danilosantos@gmail.com>
2024-11-23 21:11:30 -0300

    fix(ui): resolve bug in Sheet by adding VisuallyHidden support

--- app/_components/ui/sheet.tsx
+++ app/_components/ui/sheet.tsx
@@ -6,6 +6,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { X } from 'lucide-react';
 
 import { cn } from '@/app/_lib/utils';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 
 const Sheet = SheetPrimitive.Root;
 
@@ -60,8 +61,12 @@ const SheetContent = React.forwardRef<
         <SheetPrimitive.Content
             ref={ref}
             className={cn(sheetVariants({ side }), className)}
+            aria-describedby={undefined}
             {...props}
         >
+            <VisuallyHidden asChild>
+                <SheetPrimitive.Title />
+            </VisuallyHidden>
             <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
                 <X className="h-4 w-4" />
                 <span className="sr-only">Close</span>
