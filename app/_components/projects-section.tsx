71a9cb01776279b810af081735026ba8c1a69ae9
Danilo Santos <dev.danilosantos@gmail.com>
2024-11-26 15:52:32 -0300

    refactor: enhance projects section with project links and improve hover card content

--- app/_components/projects-section.tsx
+++ app/_components/projects-section.tsx
@@ -9,6 +9,7 @@ import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card';
 import { Badge } from './ui/badge';
 import { Arrow } from '@radix-ui/react-hover-card';
 import Link from 'next/link';
+import { Button } from './ui/button';
 
 const ProjectsSection = () => {
     const plugin = useRef(
@@ -41,53 +42,50 @@ const ProjectsSection = () => {
                                 <Card className="border-none object-cover grayscale duration-500 hover:grayscale-0">
                                     <CardContent className="flex aspect-square items-center justify-center p-6">
                                         <HoverCardTrigger asChild>
-                                            <Link
-                                                href={project.link}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                                className="group-active:cursor-grabbing"
-                                            >
-                                                <Image
-                                                    src={project.image}
-                                                    alt={project.title}
-                                                    width={500}
-                                                    height={500}
-                                                    className="h-full w-full rounded-md object-cover"
-                                                />
-                                            </Link>
+                                            <Image
+                                                src={project.image}
+                                                alt={project.title}
+                                                width={500}
+                                                height={500}
+                                                className="h-full w-full rounded-md object-cover"
+                                            />
                                         </HoverCardTrigger>
                                     </CardContent>
                                 </Card>
 
-                                <HoverCardContent className="flex w-dvw gap-2 border-none bg-black md:w-[500px]">
-                                    <Image
-                                        src={project.image}
-                                        alt={project.title}
-                                        width={50}
-                                        height={50}
-                                        className="h-[48px] w-[48px] rounded-full"
-                                    />
-
-                                    <div className="flex flex-col gap-2">
-                                        <p className="text-sm font-semibold">
+                                <HoverCardContent className="flex w-dvw flex-col gap-4 border-white/10 bg-black md:w-[500px]">
+                                    <div>
+                                        <h5 className="font-semibold">
                                             {project.title}
-                                        </p>
+                                        </h5>
                                         <span className="w-4/5 text-sm font-normal lg:w-full">
                                             {project.description}
                                         </span>
-                                        <div className="flex flex-wrap gap-2">
-                                            {project.technologies.map(
-                                                (tech) => (
-                                                    <Badge
-                                                        key={tech.id}
-                                                        className="border-border bg-white/10 text-xs font-light text-white"
-                                                    >
-                                                        {tech.name}
-                                                    </Badge>
-                                                )
-                                            )}
-                                        </div>
                                     </div>
+                                    <div className="flex flex-wrap gap-2">
+                                        {project.technologies.map((tech) => (
+                                            <Badge
+                                                key={tech.id}
+                                                className="border-border bg-white/10 text-xs font-light text-white"
+                                            >
+                                                {tech.name}
+                                            </Badge>
+                                        ))}
+                                    </div>
+                                    <Button
+                                        size="lg"
+                                        className="self-end"
+                                        asChild
+                                    >
+                                        <Link
+                                            href={project.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            Ver Projeto
+                                        </Link>
+                                    </Button>
+
                                     <Arrow width={8} height={8} />
                                 </HoverCardContent>
                             </HoverCard>
