434037634a9736dc8540fc215c7f0eb25b26f38c
Danilo Santos <dev.danilosantos@gmail.com>
2024-11-26 15:48:08 -0300

    refactor: enhance Navbar styles and import structure

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -2,12 +2,12 @@ import { SidebarTrigger } from './ui/sidebar';
 
 const Navbar = () => {
     return (
-        <nav className="fixed mb-24 flex w-dvw items-center justify-between bg-background px-5 py-4">
+        <nav className="fixed z-50 mb-24 flex w-dvw items-center justify-between bg-background p-5">
             <h6 className="text-xs font-semibold">
                 <span className="text-primary">KD</span>eveloper
             </h6>
 
-            <SidebarTrigger className="fixed right-4 top-2 rounded-full p-0 duration-0" />
+            <SidebarTrigger className="fixed right-4 top-2.5 z-50 rounded-full p-0 duration-0" />
         </nav>
     );
 };
