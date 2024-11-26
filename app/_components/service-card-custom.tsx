bf3072a591e2093c25d6786a70eb7b2252702a81
Danilo Santos <dev.danilosantos@gmail.com>
2024-11-26 15:53:51 -0300

    refactor: update service card styles and content layout

--- app/_components/service-card-custom.tsx
+++ app/_components/service-card-custom.tsx
@@ -17,13 +17,13 @@ const ServiceCardCustom = ({
     return (
         <Card className="flex h-[260px] flex-col items-center justify-center gap-3 border-border bg-white bg-opacity-5 p-6 duration-500 hover:bg-white/10">
             <CardHeader className="flex flex-col items-center gap-5 p-0">
-                <Badge className="rounded-full bg-white bg-opacity-5 p-4">
+                <Badge className="rounded-full bg-white/5 p-4 hover:bg-white/5">
                     {icon}
                 </Badge>
                 <CardTitle className="text-xl">{title}</CardTitle>
             </CardHeader>
 
-            <CardContent className="h-8 p-0 text-center text-sm">
+            <CardContent className="h-full p-0 text-center text-sm">
                 {description}
             </CardContent>
         </Card>
